cbd8346fbc153ffc901aa786098b65d0011e9915
patrickbdevaney <79435941+patrickbdevaney@users.noreply.github.com>
2021-08-26 05:09:14 -0400

    putting provider in index

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware } from 'redux'; 
+import { Provider } from 'react-redux';
+
+const store = createStore(() => [], {}, applyMiddleware());
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}><App /></Provider>, 
   document.getElementById('root')
 );
 
