9a7c122905a60979473e3398daa734b3312825e3
patrickbdevaney <79435941+patrickbdevaney@users.noreply.github.com>
2021-08-26 04:35:30 -0400

    changed mongoose connect

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
   useCreateIndex: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+ useUnifiedTopology: true,
+
 });
 
 module.exports = mongoose.connection;
