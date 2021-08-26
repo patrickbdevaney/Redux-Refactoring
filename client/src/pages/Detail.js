7dbce2664fcfa0676d4f6d5d27b28763c543afeb
patrickbdevaney <79435941+patrickbdevaney@users.noreply.github.com>
2021-08-26 03:33:28 -0400

    refactoring

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -14,10 +14,11 @@ import { QUERY_PRODUCTS } from '../utils/queries';
 import { idbPromise } from '../utils/helpers';
 import spinner from '../assets/spinner.gif';
 
-import {useDispatch, useSelector} from react-redux
+import {useDispatch, useSelector} from "react-redux"
 
 function Detail() {
- const usedispatch= useDispatch();
+ const dispatch= useDispatch();
+ const state = useSelector((state)=> state);
   const { id } = useParams();
 
   const [currentProduct, setCurrentProduct] = useState({});
